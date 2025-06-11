95756f09d3e76b671348d89d5004d5415a0b2a72
Florian <pezf68@gmail.com>
2025-06-11 16:13:05 +0200

    use gemini instead of claude + add instructions as param

--- agent.py
+++ agent.py
@@ -1,12 +1,25 @@
 from agno.agent import Agent
-from agno.models.anthropic import Claude
+from agno.models.google import Gemini
+from langfuse import observe, get_client
 
-agent = Agent(
-    model=Claude(id="claude-sonnet-4-20250514"),
-    description="Tu es un créateur de comptes pour enfant",
-    instructions=["Crée une histoire courte (entre 200 et 400 mots) basés sur le thème qui te va être communiqué par un enfant"],
-    markdown=True,
-    debug_mode=True
-)
 
-agent.print_response("Raconte moi une histoire de princesse.", stream=True)
+@observe()
+def run_agent(user_prompt, instructions):
+    langfuse = get_client()
+
+    langfuse.update_current_trace(
+        name="Story Generation",
+        tags=["ext_eval_pipelines"]
+    )
+    agent = Agent(
+        model=Gemini(id="gemini-2.0-flash"),
+        description="Tu es un créateur de comptes pour enfant",
+        instructions=[instructions],
+        markdown=True,
+        debug_mode=True
+    )
+    result = agent.run(user_prompt)
+
+    return result
+
+# run_agent("Une histoire sur un dragon gentil qui a peur des souris.", "Crée une histoire courte (entre 200 et 400 mots) basés sur le thème qui te va être communiqué par un enfant")
